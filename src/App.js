8a6f51123774d94a25e0d8a44ed8b029d107efbc
Larissa Ferreira <larissadantasaf@gmail.com>
2022-03-08 14:27:50 +0100

    Fix merged conflict

--- src/App.js
+++ src/App.js
@@ -7,14 +7,10 @@ import Login from "./pages/auth/Login";
 import ProtectedRoute from "./pages/auth/ProtectedRoute";
 import BusinessDashboard from "./pages/BusinessDashboard/BusinessDashboard";
 
-<<<<<<< HEAD:src/App.js
 import { AuthContextComponent } from "./contexts/authContext";
-=======
+
 import { NavBar } from "./components/NavBar/NavBar";
 
-
-import { AuthContextComponent } from "../contexts/authContext";
->>>>>>> c5e70531d911b02988cc578276f108cf3ba2e36b:src/components/App.js
 
 function App() {
   return (
@@ -23,11 +19,9 @@ function App() {
         <Route path="/" element={<ProtectedRoute component={Home} />} />
         <Route path="/signup" element={<Signup />} />
         <Route path="/login" element={<Login />} />
-<<<<<<< HEAD:src/App.js
         <Route path="/businessdashboard" element={<BusinessDashboard />} />
-=======
         <Route path="/navbar" element={<NavBar />} />
->>>>>>> c5e70531d911b02988cc578276f108cf3ba2e36b:src/components/App.js
+
       </Routes>
     </AuthContextComponent>
   );
