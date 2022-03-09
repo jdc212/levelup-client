fc37c7782f8858dd4d972129df74ef7e06abca35
Thais Tamae <thais.tamae@gmail.com>
2022-03-08 21:28:56 -0300

    add form user

--- src/pages/BusinessDashboard/BusinessDashboard.js
+++ src/pages/BusinessDashboard/BusinessDashboard.js
@@ -1,55 +1,60 @@
 import { useNavigate } from "react-router-dom";
 import { useState, useEffect } from "react";
 import { ClientList } from "../../components/ClientList/ClientList";
+import { FormAddUser } from "../../components/FormAddUser/FormAddUser";
 import api from "../../apis/api";
 
-export default function BusinessDashboard(){
-
+export default function BusinessDashboard() {
   const navigate = useNavigate();
 
-  const [programs, setPrograms] = useState([])
-  const [users, setUsers] = useState([])
-  const [form, setForm] = useState([])
+  const [programs, setPrograms] = useState([]);
+  const [users, setUsers] = useState([]);
+  const [form, setForm] = useState([]);
+  const [loading, setLoading] = useState(true);
 
-  console.log(setPrograms)
+  console.log(setPrograms);
 
   useEffect(() => {
     async function fetchPrograms() {
       try {
         const response = await api.get("/points/my-points");
-        console.log(response.data)
-        setPrograms([...response.data]);
-        console.log(programs)
+        setPrograms([response.data]);
+        console.log(programs);
       } catch (error) {
         console.error(error);
       }
     }
     fetchPrograms();
-  }, [programs]);
+  }, []);
 
-  useEffect(() => {
+  {
+    /*useEffect(() => {
     async function fetchUserPoints() {
       try {
-        const response = await api.get("/user-points/user-points");
-        console.log(...response.data)
-        setUsers([...response.data]);
+        const response2 = await api.get("/user-points/user-points");
+        console.log(response2.data)
+        setUsers([...response2.data]);
         console.log(users)
+
       } catch (error) {
         console.error(error);
       }
     }
     fetchUserPoints();
-  }, [users]);
+  }, [users]);*/
+  }
 
-   
   function handleChange(event) {
     setForm({ ...form, [event.target.name]: event.target.value });
-  } 
+  }
 
   async function handleSubmitCredit(event) {
     event.preventDefault();
     try {
-      const response = await api.post("/:pointId/add-points/:userPointsId", form);
+      const response = await api.post(
+        "/:pointId/add-points/:userPointsId",
+        form
+      );
       console.log(response.data);
       navigate("/businessdashboard");
     } catch (error) {
@@ -60,7 +65,10 @@ export default function BusinessDashboard(){
   async function handleSubmitCompensate(event) {
     event.preventDefault();
     try {
-      const response = await api.post("/:pointId/credit-points/:userPointsId", form);
+      const response = await api.post(
+        "/:pointId/credit-points/:userPointsId",
+        form
+      );
       console.log(response.data);
       navigate("/businessdashboard");
     } catch (error) {
@@ -68,10 +76,11 @@ export default function BusinessDashboard(){
     }
   }
 
-
-  return(
+  return (
     <div>
-       {users.map((current) => {
+      <FormAddUser/>
+
+      {/*{users.map((current) => {
         return <ClientList
             key={current._id}
             customerEmail= {current.customerEmail}
@@ -81,8 +90,7 @@ export default function BusinessDashboard(){
             handleSubmitCredit={handleSubmitCredit}
             handleSubmitCompensate={handleSubmitCompensate}
            />
-       })}
+       })}*/}
     </div>
-  )
-
+  );
 }
