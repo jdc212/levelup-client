f0986539a9fd1c1418d4bc5c4f8ff2ce46134351
Larissa Ferreira <larissadantasaf@gmail.com>
2022-03-10 19:36:32 +0100

    Add route for forgotpassword

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ import BusinessDashboard from "./pages/BusinessDashboard/BusinessDashboard";
 import { AuthContextComponent } from "./contexts/authContext";
 import { NavBar } from "./components/NavBar/NavBar";
 import BusinessProfileInfo from "./components/BusinessProfileInfo/BusinessProfileInfo";
+import ForgotPassword from "./pages/auth/ForgotPassword";
 
 function App() {
   return (
@@ -19,6 +20,7 @@ function App() {
         <Route path="/login" element={<Login />} />
         <Route path="/businessprofileinfo" element={<BusinessProfileInfo />} />
         <Route path="/businessdashboard" element={<BusinessDashboard />} />
+        <Route path="/forgot-password" element={<ForgotPassword />} />
         
       </Routes>
     </AuthContextComponent>
