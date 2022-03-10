c628cd976ba905f4f377b1e8315273bdfccd0563
Larissa Ferreira <larissadantasaf@gmail.com>
2022-03-10 01:21:34 +0100

    Add new typos

--- src/pages/auth/NewInput.js
+++ src/pages/auth/NewInput.js
@@ -2,13 +2,16 @@ import React from "react";
 
 function newInput(props) {
 	return (
-		<>
+		<>	
+			<label htmlFor={props.id}>{props.label}</label>
 			<input
 				name={props.name}
+				id={props.id}
 				type={props.type}
 				placeholder={props.label}
 				value={props.value}
 				onChange={props.onChange}
+				required={props.required}
 			/>
 		</>
 	);
