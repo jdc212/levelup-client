a52d06fc16b23db0f91b6e5003ca81bc65a17da0
Larissa Ferreira <larissadantasaf@gmail.com>
2022-03-17 18:42:38 +0100

    Fix buttom shadow

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -68,7 +68,7 @@ function Home() {
                   Create the most powerful fidelity program to your business
                 </p>
                 <div className="mt-5 sm:mt-8 sm:flex sm:justify-center lg:justify-start">
-                  <div className="rounded-md shadow">
+                  <div className="mt-3 sm:mt-0 sm:ml-3">
                     <Link
                       to="/"
                       className="w-full flex items-center justify-center px-8 py-3 border border-transparent text-base font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 md:py-4 md:text-lg md:px-10"
