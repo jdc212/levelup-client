d3917fcb659f7a70efd6363f0c7c60f7b43f71f0
Larissa Ferreira <larissadantasaf@gmail.com>
2022-03-17 17:33:41 +0100

    Add newpassword style

---
+++ src/pages/auth/NewPassword.js
@@ -0,0 +1,163 @@
+import { useFormik } from "formik";
+import { Link, useNavigate, useParams } from "react-router-dom";
+import api from "../../apis/api";
+import { useState } from "react";
+import * as Yup from "yup";
+import toast, { Toaster } from "react-hot-toast";
+import { LockClosedIcon } from "@heroicons/react/solid";
+
+export default function NewPassword() {
+  const [loading, setLoading] = useState(false);
+  const params = useParams();
+
+  const navigate = useNavigate();
+
+  const formik = useFormik({
+    initialValues: {
+      newPassword: "",
+      confirmPassword: "",
+    },
+    validationSchema: Yup.object({
+      newPassword: Yup.string()
+        .matches(
+          /^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$ %^&*-]).{8,}$/,
+          "Password is required and must have at least 8 characters, uppercase and lowercase letters, numbers and special characters."
+        )
+        .required("Field is required."),
+    }),
+    onSubmit: (values) => {
+      async function login() {
+        try {
+          setLoading(true);
+
+          await api.put(`/password/reset-password/${params.token}`, values);
+          setLoading(false);
+          toast.success("Password updated!");
+
+          setTimeout(() => {
+            navigate("/login");
+          }, 3000);
+        } catch (e) {
+          setLoading(false);
+          console.error(e);
+        }
+      }
+      login();
+    },
+  });
+
+  return (
+    <div className="h-full w-full bg-gray-50">
+      <div className="min-h-full flex items-center justify-center py-12 px-4 sm:px-6 lg:px-8">
+        <div className="max-w-md w-full space-y-8">
+          <div>
+            <img
+              className="mx-auto h-12 w-auto"
+              src="https://tailwindui.com/img/logos/workflow-mark-indigo-600.svg"
+              alt="Workflow"
+            />
+            <h2 className="mt-6 text-center text-3xl font-extrabold text-gray-900">
+              Get the new password
+            </h2>
+            <p className="mt-2 text-center text-sm text-gray-600">
+              Or{" "}
+              <Link
+                to="/signup"
+                className="font-medium text-indigo-600 hover:text-indigo-500"
+              >
+                click here to signup!
+              </Link>
+            </p>
+          </div>
+          <form className="mt-8 space-y-6" onSubmit={formik.handleSubmit}>
+            <input type="hidden" name="remember" defaultValue="true" />
+            <div className="rounded-md shadow-sm -space-y-px">
+              <div>
+                <label htmlFor="password" className="sr-only">
+                  Password
+                </label>
+                <input
+                  id="password"
+                  name="password"
+                  type="password"
+                  autoComplete="current-password"
+                  required
+                  className="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-b-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm"
+                  placeholder="Password"
+                  value={formik.values.password}
+                  error={formik.errors.password}
+                  onChange={formik.handleChange}
+                />
+              </div>
+
+              {formik.touched.password && formik.errors.password ? (
+                <div>{formik.errors.password}</div>
+              ) : null}
+            </div>
+
+            <div>
+              <label htmlFor="password" className="sr-only">
+                Confirm password
+              </label>
+              <input
+                id="password"
+                name="confirmPassword"
+                type="password"
+                autoComplete="current-password"
+                required
+                className="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-b-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm"
+                placeholder="Password"
+                value={formik.values.confirmPassword}
+                error={formik.errors.password}
+                onChange={formik.handleChange}
+              />
+            </div>
+
+            {formik.touched.confirmPassword && formik.errors.confirmPassword ? (
+              <div>{formik.errors.confirmPassword}</div>
+            ) : null}
+
+            <div>
+              <button
+                type="submit"
+                disabled={loading}
+                className="group relative w-full flex justify-center py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+              >
+                <span className="absolute left-0 inset-y-0 flex items-center pl-3">
+                  <LockClosedIcon
+                    className="h-5 w-5 text-indigo-500 group-hover:text-indigo-400"
+                    aria-hidden="true"
+                  />
+                </span>
+                New password
+              </button>
+            </div>
+          </form>
+          <Toaster
+            position="top-center"
+            reverseOrder={false}
+            gutter={8}
+            containerClassName=""
+            containerStyle={{}}
+            toastOptions={{
+              className: "",
+              duration: 5000,
+              style: {
+                background: "#fff",
+                color: "#000",
+              },
+
+              success: {
+                duration: 3000,
+                theme: {
+                  primary: "green",
+                  secondary: "black",
+                },
+              },
+            }}
+          />
+        </div>
+      </div>
+    </div>
+  );
+}
