dc8bd8ff4ed0795158ca4da50ad528bf289d6631
Larissa Ferreira <larissadantasaf@gmail.com>
2022-03-17 17:35:22 +0100

    Add new home style

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,65 +1,169 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import logo from '../../assets/styles/LevelUpWhite.png'
-
-import styles from "./Home.module.css";
+import logo from "../../assets/styles/v1069-011.jpg";
+import { Fragment } from "react";
+import { Popover, Transition } from "@headlessui/react";
+import { MenuIcon } from "@heroicons/react/outline";
+import {
+  AnnotationIcon,
+  GlobeAltIcon,
+  LightningBoltIcon,
+} from "@heroicons/react/outline";
 
 function Home() {
   return (
-    <div>
-      {/*imageHero*/}
-      <section>
-        <div className={styles.sectionhero}>
-          <div>
-            <img className={styles.logo} src={logo} />
-            <p className={styles.textImageHero}>
-            Transform your business into an experience
+    <>
+      <div className="relative bg-white overflow-hidden">
+        <div className="max-w-7xl mx-auto">
+          <div className="relative z-10 pb-8 bg-white sm:pb-16 md:pb-20 lg:max-w-2xl lg:w-full lg:pb-28 xl:pb-32">
+            <svg
+              className="hidden lg:block absolute right-0 inset-y-0 h-full w-48 text-white transform translate-x-1/2"
+              fill="currentColor"
+              viewBox="0 0 100 100"
+              preserveAspectRatio="none"
+              aria-hidden="true"
+            >
+              <polygon points="50,0 100,0 50,100 0,100" />
+            </svg>
+
+            <Popover>
+              <div className="relative pt-6 px-4 sm:px-6 lg:px-8">
+                <nav
+                  className="relative flex items-center justify-between sm:h-10 lg:justify-start"
+                  aria-label="Global"
+                >
+                  <div className="flex items-center flex-grow flex-shrink-0 lg:flex-grow-0">
+                    <div className="flex items-center justify-between w-full md:w-auto">
+                      <div className="-mr-2 flex items-center md:hidden">
+                        <Popover.Button className="bg-white rounded-md p-2 inline-flex items-center justify-center text-gray-400 hover:text-gray-500 hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-indigo-500">
+                          <span className="sr-only">Open main menu</span>
+                          <MenuIcon className="h-6 w-6" aria-hidden="true" />
+                        </Popover.Button>
+                      </div>
+                    </div>
+                  </div>
+                </nav>
+              </div>
+
+              <Transition
+                as={Fragment}
+                enter="duration-150 ease-out"
+                enterFrom="opacity-0 scale-95"
+                enterTo="opacity-100 scale-100"
+                leave="duration-100 ease-in"
+                leaveFrom="opacity-100 scale-100"
+                leaveTo="opacity-0 scale-95"
+              ></Transition>
+            </Popover>
+
+            <main className="mt-10 mx-auto max-w-7xl px-4 sm:mt-12 sm:px-6 md:mt-16 lg:mt-20 lg:px-8 xl:mt-28">
+              <div className="sm:text-center lg:text-left">
+                <h1 className="text-4xl tracking-tight font-extrabold text-gray-900 sm:text-5xl md:text-6xl">
+                  <span className="block xl:inline">Level Up</span>{" "}
+                  <span className="block text-indigo-600 xl:inline">
+                    Transform your business into an experience
+                  </span>
+                </h1>
+                <p className="mt-3 text-base text-gray-500 sm:mt-5 sm:text-lg sm:max-w-xl sm:mx-auto md:mt-5 md:text-xl lg:mx-0">
+                  Create the most powerful fidelity program to your business
+                </p>
+                <div className="mt-5 sm:mt-8 sm:flex sm:justify-center lg:justify-start">
+                  <div className="rounded-md shadow">
+                    <Link
+                      to="/"
+                      className="w-full flex items-center justify-center px-8 py-3 border border-transparent text-base font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 md:py-4 md:text-lg md:px-10"
+                    >
+                      Get started
+                    </Link>
+                  </div>
+                  <div className="mt-3 sm:mt-0 sm:ml-3">
+                    <Link
+                      to="/"
+                      className="w-full flex items-center justify-center px-8 py-3 border border-transparent text-base font-medium rounded-md text-indigo-700 bg-indigo-100 hover:bg-indigo-200 md:py-4 md:text-lg md:px-10"
+                    >
+                      Discovery more
+                    </Link>
+                  </div>
+                </div>
+              </div>
+            </main>
+          </div>
+        </div>
+        <div className="lg:absolute lg:inset-y-0 lg:right-0 lg:w-1/2">
+          <img
+            className="h-56 w-full object-cover sm:h-72 md:h-96 lg:w-full lg:h-full"
+            src={logo}
+            alt=""
+          />
+        </div>
+      </div>
+
+      <div className="py-12 bg-white">
+        <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+          <div className="lg:text-center">
+            <h2 className="text-base text-indigo-600 font-semibold tracking-wide uppercase">
+              fidelity program
+            </h2>
+            <p className="mt-2 text-3xl leading-8 font-extrabold tracking-tight text-gray-900 sm:text-4xl">
+              A better way to your business
+            </p>
+            <p className="mt-4 max-w-1xl text-xl text-gray-500 lg:mx-auto">
+              Level Up is a loyalty program provider and a strategic technology
+              partner that empowers retailers to improve their performance and
+              offer their shoppers an unique experience, through reward program
+              solutions, and complementary marketing services.
             </p>
           </div>
-        </div>
-      </section>
 
-      <section className={styles.sectionfaq}>
-        <div className={styles.containerfull}>
-          <div className={styles.faqtitlebox}>
-            <div className={styles.wcontainer}>
-              <h2 className={styles.title}>
-                Create the most powerful <br></br>fidelity program to your business
-              </h2>
-            <div className={styles.faqbox}>
-                <div className={styles.faqitem}>
-                  <h3 className={styles.faqitemtrigger}>
+          <div className="mt-10">
+            <dl className="space-y-10 md:space-y-0 md:grid md:grid-cols-2 md:gap-x-8 md:gap-y-10">
+              <div className="relative">
+                <dt>
+                  <div className="absolute flex items-center justify-center h-12 w-12 rounded-md bg-indigo-500 text-white">
+                    <LightningBoltIcon className="h-6 w-6" aria-hidden="true" />
+                  </div>
+                  <p className="ml-16 text-lg leading-6 font-medium text-gray-900">
                     Data-driven promotions
-                  </h3>
-                  <p className={styles.faqitemcontentinner}>
-                    Sync all your data in real-time to personalize your
-                    promotions and boost your performance.
                   </p>
-                </div>
-                <div className={styles.faqitem}>
-                  <h3 className={styles.faqitemtrigger}>One single UI</h3>
-                  <p className={styles.faqitemcontentinner}>
-                    In the Campaign Manager marketers can create sophisticated
-                    promotions without blocking developers.
+                </dt>
+                <dd className="mt-2 ml-16 text-base text-gray-500">
+                  Sync all your data in real-time to personalize your promotions
+                  and boost your performance.
+                </dd>
+              </div>
+              <div className="relative">
+                <dt>
+                  <div className="absolute flex items-center justify-center h-12 w-12 rounded-md bg-indigo-500 text-white">
+                    <AnnotationIcon className="h-6 w-6" aria-hidden="true" />
+                  </div>
+                  <p className="ml-16 text-lg leading-6 font-medium text-gray-900">
+                    One single UI
                   </p>
-                </div>
-                <div className={styles.faqitem}>
-                  <h3 className={styles.faqitemtrigger}>
+                </dt>
+                <dd className="mt-2 ml-16 text-base text-gray-500">
+                  In the Campaign Manager marketers can create sophisticated
+                  promotions without blocking developers.
+                </dd>
+              </div>
+              <div className="relative">
+                <dt>
+                  <div className="absolute flex items-center justify-center h-12 w-12 rounded-md bg-indigo-500 text-white">
+                    <GlobeAltIcon className="h-6 w-6" aria-hidden="true" />
+                  </div>
+                  <p className="ml-16 text-lg leading-6 font-medium text-gray-900">
                     Cloud native and extendable
-                  </h3>
-                  <p className={styles.faqitemcontentinner}>
-                    Talon.One is endlessly scalable. Connect your customer data,
-                    integrate with any 3rd parties and optimize on the fly.
                   </p>
-                </div>
-            </div>
-
-            <Link className={styles.signupbutton} to="/signup">Signup here!</Link>
-          </div>
+                </dt>
+                <dd className="mt-2 ml-16 text-base text-gray-500">
+                  Talon. One is endlessly scalable. Connect your customer data,
+                  integrate with any 3rd parties and optimize on the fly.
+                </dd>
+              </div>
+            </dl>
           </div>
         </div>
-      </section>
-    </div>
+      </div>
+    </>
   );
 }
 
