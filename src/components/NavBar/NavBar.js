a06d2525f5f9af37a2a08207044ea691f169e4ab
Larissa Ferreira <larissadantasaf@gmail.com>
2022-03-17 17:32:05 +0100

    Add new navbar style

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -1,45 +1,70 @@
 import { Link } from "react-router-dom";
-import styles from "./NavBar.module.css";
+import { Popover } from "@headlessui/react";
+import { MenuIcon } from "@heroicons/react/outline";
 
 export function NavBar() {
-    return (
-        <div className={styles.topbar}>
-            <div >
-                <div className={styles.firstline}>
-                <div className={styles.pages}>
-                    <Link to="/" className={styles.linkto}>
-                        <div className="navBar-icon">
-                            <span className={styles.textnavbar}>Business</span>
-                        </div>
-                    </Link>
-                    <Link to="/clients" className={styles.linkto}>
-                        <div className="navBar-icon">
-                            <span className={styles.textnavbar}>Clients</span>
-                        </div>
-                    </Link>
-                    <Link to="/corporate" className={styles.linkto}>
-                        <div className="navBar-icon">
-                            <span className={styles.textnavbar}>Corporate</span>
-                        </div>
-                        
-                    </Link>
-                </div>
-                <div className={styles.pages}>
-                    <Link to="/login" className={styles.linkto}>
-                        <div className="navBar-icon">
-                            <span className={styles.textnavbar}>Login</span>
-                        </div>
-                    </Link>
-                    <Link to="/businessprofileinfo" className={styles.linkto}>
-                        <div className="navBar-icon">
-                            <span className={styles.textnavbar}>Business Profile</span>
-                        </div>
-                    </Link>
-                </div>
+  return (
+    <Popover className="relative bg-white">
+      <div className="max-w-7xl mx-auto px-4 sm:px-6">
+        <div className="flex justify-between items-center border-b-2 border-gray-100 py-6 md:justify-start md:space-x-10">
+          <div className="flex justify-start lg:w-0 lg:flex-1">
+            <Link to="/">
+              <span className="sr-only">Workflow</span>
+              <img
+                className="h-8 w-auto sm:h-10"
+                src="https://tailwindui.com/img/logos/workflow-mark-indigo-600.svg"
+                alt=""
+              />
+            </Link>
+          </div>
+          <div className="-mr-2 -my-2 md:hidden">
+            <Popover.Button className="bg-white rounded-md p-2 inline-flex items-center justify-center text-gray-400 hover:text-gray-500 hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-indigo-500">
+              <span className="sr-only">Open menu</span>
+              <MenuIcon className="h-6 w-6" aria-hidden="true" />
+            </Popover.Button>
+          </div>
 
-                </div>
-            </div>
+          <Link
+            to="/"
+            className="text-base font-medium text-gray-500 hover:text-gray-900"
+          >
+            Business
+          </Link>
+          <Link
+            to="/clients"
+            className="text-base font-medium text-gray-500 hover:text-gray-900"
+          >
+            Clients
+          </Link>
+          <Link
+            to="/corporate"
+            className="text-base font-medium text-gray-500 hover:text-gray-900"
+          >
+            Corporate
+          </Link>
+          <Link
+            to="/businessprofileinfo"
+            className="text-base font-medium text-gray-500 hover:text-gray-900"
+          >
+            Business Profile
+          </Link>
+
+          <div className="hidden md:flex items-center justify-end md:flex-1 lg:w-0">
+            <Link
+              to="/login"
+              className="whitespace-nowrap text-base font-medium text-gray-500 hover:text-gray-900"
+            >
+              Sign in
+            </Link>
+            <Link
+              to="/signup"
+              className="ml-8 whitespace-nowrap inline-flex items-center justify-center px-4 py-2 border border-transparent rounded-md shadow-sm text-base font-medium text-white bg-indigo-600 hover:bg-indigo-700"
+            >
+              Sign up
+            </Link>
+          </div>
         </div>
-    )
-        
-};
+      </div>
+    </Popover>
+  );
+}
