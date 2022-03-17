e78d63bcb3e36c35092871c9171932bdc89fdda8
Joe D <joedeejr@gmail.com>
2022-03-17 15:18:24 -0300

    Perfecting businessProfileInfo + clientList

--- src/components/ClientList/ClientList.js
+++ src/components/ClientList/ClientList.js
@@ -45,26 +45,29 @@ export function ClientList(props) {
   }
 
   return (
-    <div className="max-w-sm bg-white overflow-hidden shadow sm:rounded-md">
+    <div className="max-w-full bg-white overflow-hidden shadow sm:rounded-md">
       <div className="block text-sm font-medium text-gray-700">
         <h4 className="">Customer Points</h4>
         <div className="">
         <div className="">
-          <div className="mt-1 w-130 flex rounded-md shadow-sm">
+          <div className="mt-1 w-130 flex rounded-md shadow-sm grid grid-cols-3 gap-6">
             <section>
-              <p className="mt-1 flex rounded-md shadow-sm">Customer Email: {props.customerEmail}</p>
-              <p className="mt-1 flex rounded-md shadow-sm">Points: {props.pointsAccumulated}</p>
+              <p className="block text-indigo-600 xl">Customer Email: {props.customerEmail}</p>
+              <p className="block text-indigo-600">Points: {props.pointsAccumulated}</p>
             </section>
 
-            <section className="">
+            <section >
               <form onSubmit={handleSubmitCredit} className="block text-sm font-medium text-gray-700">
                 <h4>Credit Points</h4>
-                <label htmlFor="credit">Choose Program</label>
+                <label htmlFor="credit" >Choose Program</label>
                 <br></br>
                 <select
                   id="credit"
                   name="points"
+                  /*className="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 cursor-default placeholder-gray-500 text-gray-900 rounded-b-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm pr-2"*/
+                  className="relative w-full bg-white border border-gray-300 rounded-md pl-3 pr-10 py-2 text-left cursor-default focus:outline-none focus:ring-1 focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
                   value={credit.points}
+                  
                   onChange={handleChangeCredit}
                 >
                   <option value=""></option>
@@ -78,7 +81,7 @@ export function ClientList(props) {
                     );
                   })}
                 </select>
-
+                    <br></br>
                 <button 
                 type="submit" className="inline-flex justify-center py-2 px-4 border border-transparent shadow-sm text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
                 >
@@ -86,6 +89,9 @@ export function ClientList(props) {
                 </button>
               </form>
 
+            </section>
+
+            <section>
               <form
                 className="block text-sm font-medium text-gray-700"
                 onSubmit={handleSubmitCompensate}
@@ -95,9 +101,11 @@ export function ClientList(props) {
                 <input
                   id="number"
                   name="number"
+                  className="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-b-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm"
                   value={props.number}
                   onChange={handleChangeCompensate}
                 />
+                <br></br>
                 <button 
                 type="submit" 
                 className="inline-flex justify-center py-2 px-4 border border-transparent shadow-sm text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
