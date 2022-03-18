309301f805021fb81c30a9fab967d4dc373d30ce
Thais Tamae <thais.tamae@gmail.com>
2022-03-18 16:52:24 -0300

    navbar clients

--- src/pages/Clients/Clients.js
+++ src/pages/Clients/Clients.js
@@ -5,10 +5,13 @@ import {
   GlobeIcon,
   GiftIcon,
 } from "@heroicons/react/outline";
+import { NavBar } from "../../components/NavBar/NavBar";
+
 
 function Corporate() {
   return (
     <>
+    <NavBar />
       <div className="bg-gray-50">
         <div className="max-w-7xl mx-auto py-12 px-4 sm:px-6 lg:py-16 lg:px-8 lg:flex lg:items-center lg:justify-between">
           <h2 className="text-3xl font-extrabold tracking-tight text-gray-900 sm:text-4xl">
