df8eb5b61b79b008d3863f04060eb6d2dbc26421
Thais Tamae <thais.tamae@gmail.com>
2022-03-17 23:14:21 -0300

    footer v2

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -4,8 +4,8 @@ import { Link } from "react-router-dom";
 export function Footer() {
   return (
       
-      <div className="bg-gray-50">
-        <div className="max-w-7xl mx-auto py-12 px-4 sm:px-6 lg:py-16 lg:px-8 lg:flex lg:items-center lg:justify-between">
+      <div className="bg-white-50">
+        <div className="max-w-7xl border-t-2 border-gray-100 mx-auto py-12 px-4 sm:px-6 lg:py-16 lg:px-8 lg:flex lg:items-center lg:justify-between">
           <h2 className="text-3xl font-extrabold tracking-tight text-gray-900 sm:text-4xl">
             <span className="block">Ready to dive in?</span>
             <span className="pt-3 block text-indigo-600">Join the loyalty revolution</span>
@@ -13,8 +13,7 @@ export function Footer() {
           <div className="mt-8 flex lg:mt-0 lg:flex-shrink-0">
             <div className="inline-flex rounded-md">
               <h2 className="text-5xl font-extrabold tracking-tight text-gray-900 sm:text-4xl">
-                <img src="../../assets/styles/resised.png" className="h-5" alt=""/>
-                
+                <p className="text-indigo-600 text-5xl">Level Up</p>              
               </h2>
             </div>
           </div>
@@ -41,9 +40,9 @@ export function Footer() {
                   </Link>
           </div>
           </div>
-          <div className="m-0 px-0 bg-gray-200">
+          <div className="m-0 px-0 bg-white-50">
           <div className="py-6 px-8 md:flex md:items-center md:justify-between ">
-            <span className="px-8 text-sm text-gray-700 sm:text-center dark:text-gray-400">© 2022 <a href="https://https://leveluployalty.netlify.app/" class="hover:underline">LevelUp™</a>. All Rights Reserved.
+            <span className="px-8 text-sm text-gray-500 sm:text-center dark:text-gray-400">© 2022 <a href="https://https://leveluployalty.netlify.app/" class="hover:underline">LevelUp™</a>. All Rights Reserved.
             </span>
           </div>
           </div>
