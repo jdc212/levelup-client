ea3cbabfbeb42f011a4e7e753f2255f9bd0f8f3e
Larissa Ferreira <larissadantasaf@gmail.com>
2022-03-31 16:11:59 +0200

    Fix error programs, style and disable react-hooks/exhaustive-deps

--- src/pages/BusinessDashboard/BusinessDashboard.js
+++ src/pages/BusinessDashboard/BusinessDashboard.js
@@ -7,7 +7,6 @@ import { SearchBar } from "../../components/SearchBar/SearchBar";
 import api from "../../apis/api";
 import DashboardNavBar from "./DashboardNavBar";
 
-
 export default function BusinessDashboard() {
   const [programs, setPrograms] = useState([]);
   const [users, setUsers] = useState([]);
@@ -16,13 +15,12 @@ export default function BusinessDashboard() {
   const [backup, setBackup] = useState([]);
   /* const [isCredited, setIsCredited] = useState(false); */
 
-  console.log(setPrograms);
-
   useEffect(() => {
     async function fetchPrograms() {
       try {
+        // api.get("/points/my-points").then(response => setPrograms(response.data));
         const response = await api.get("/points/my-points");
-        setPrograms([...response.data]);
+        setPrograms(response.data);
         setReload(false);
         console.log(programs);
       } catch (error) {
@@ -30,15 +28,15 @@ export default function BusinessDashboard() {
       }
     }
     fetchPrograms();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [reload]);
 
   useEffect(() => {
     async function fetchUserPoints() {
       try {
-        const response2 = await api.get("/user-points/user-points");
-        console.log(response2.data);
-        setBackup([...response2.data]);
-        setUsers([...response2.data.reverse()]);
+        const response = await api.get("/user-points/user-points");
+        setBackup([...response.data]);
+        setUsers([...response.data.reverse()]);
         setReload(false);
         console.log(users);
       } catch (error) {
@@ -46,7 +44,12 @@ export default function BusinessDashboard() {
       }
     }
     fetchUserPoints();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [reload]);
+
+  const handleProgramAdded = (program) => {
+    setPrograms((prevPrograms) => [...prevPrograms, program]);
+  };
 
   function filterUserEmail(searchParams) {
     if (searchParams === "") {
@@ -65,113 +68,82 @@ export default function BusinessDashboard() {
 
   return (
     <>
-      <DashboardNavBar/>
+      <DashboardNavBar />
       <div className="px-16 bg-gray-50">
-        <div className="md:col-span-1">
-          <div className="pt-4 pb-0 sm:px-0">
-            <h3 className="text-lg font-medium leading-6 text-gray-900">
-              Create Promotion
-            </h3>
-          </div>
-        </div>
+        <FormAddPromo onSubmit={handleProgramAdded} />
 
-        <div className="flex justify-center">
-          <FormAddPromo />
-        </div>
-
-        <div className="hidden sm:block" aria-hidden="true">
+        <div className="sm:block" aria-hidden="true">
           <div className="py-4">
             <div className="border-t border-gray-200" />
           </div>
         </div>
 
-        <div className="md:col-span-1">
-          <div className="pb-2 sm:px-0">
-            <h3 className="text-lg font-medium leading-6 text-gray-900">
-              Registered Promotions
-            </h3>
-          </div>
-        </div>
-        <div className="flex justify-center">
-          <div className="grid grid-cols-3 grid-rows-3 gap-6">
-            {programs.map((current) => {
-              return (
-                <div className="px-3">
-                  <CardPromoList
-                    key={current._id}
-                    id={current._id}
-                    creditSystem={current.creditSystem}
-                    launch={current.launch}
-                    deadline={current.deadline}
-                    service={current.service}
-                    reloadPage={setReload}
-                  />
+        <div className="bg-gray-50">
+          <div className="mt-10 sm:mt-0">
+            <div className="md:grid md:grid-cols-2 md:gap-6">
+              <div className="md:col-span-1">
+                <div className="px-4 sm:px-0">
+                  <h3 className="text-lg font-medium leading-6 text-gray-900" id="promotions">
+                    Registered Promotions
+                  </h3>
+                  <p className="mt-1 text-sm text-gray-600">
+                    Use a permanent address where you can receive mail.
+                  </p>
                 </div>
-              );
-            })}
+              </div>
+
+              <div className="flex justify-center">
+                <div className="grid grid-cols-3 grid-rows-3 gap-6">
+                  {programs.map((current) => {
+                    return (
+                      <CardPromoList
+                        key={current._id}
+                        id={current._id}
+                        creditSystem={current.creditSystem}
+                        launch={current.launch}
+                        deadline={current.deadline}
+                        service={current.service}
+                        reloadPage={setReload}
+                        className="px-3"
+                      />
+                    );
+                  })}
+                </div>
+              </div>
+            </div>
           </div>
         </div>
 
-        <div className="hidden sm:block" aria-hidden="true">
+        <FormAddUser />
+
+        <div className="sm:block" aria-hidden="true">
           <div className="py-4">
             <div className="border-t border-gray-200" />
           </div>
         </div>
 
-        <div className="md:col-span-1">
-          <div className="pb-0 sm:px-0">
-            <h3 className="text-lg font-medium leading-6 text-gray-900">
-              Create Customer Points
-            </h3>
-          </div>
-        </div>
+        <SearchBar placeholder="Search" filterAPI={filterUserEmail} />
 
-        <div>
-          <div className="flex justify-center">
-            <FormAddUser />
-          </div>
-
-          <div className="hidden sm:block" aria-hidden="true">
-            <div className="py-4">
-              <div className="border-t border-gray-200" />
-            </div>
-          </div>
-
-          <div className="md:col-span-1">
-            <div className="pb-0 sm:px-0">
-              <h3 className="text-lg font-medium leading-6 text-gray-900">
-                Search Customer Profile
-              </h3>
-            </div>
-          </div>
-
-          
-          <div className="mt-8 px-20">
-            <SearchBar placeholder="Search" filterAPI={filterUserEmail} />
-          </div>
-          
-          
-          {user.map((current) => {
-            return (
-              <ClientList
-                key={current._id}
-                id={current._id}
-                customerEmail={current.customerEmail}
-                pointsAccumulated={current.pointsAccumulated}
-                services={programs.map((program) => {
-                  return {
-                    service: program.service,
-                    creditSystem: program.creditSystem,
-                  };
-                })}
-                reloadPage={setReload}
-                /*isCredited={setIsCredited}
-            creditState={isCredited}
-            filterUserEmail={filterUserEmail}*/
-              />
-            );
-          })}
-        </div>
+        {user.map((current) => {
+          return (
+            <ClientList
+              key={current._id}
+              id={current._id}
+              customerEmail={current.customerEmail}
+              pointsAccumulated={current.pointsAccumulated}
+              services={programs.map((program) => {
+                return {
+                  service: program.service,
+                  creditSystem: program.creditSystem,
+                };
+              })}
+              reloadPage={setReload}
+              /*isCredited={setIsCredited}
+          creditState={isCredited}
+          filterUserEmail={filterUserEmail}*/
+            />
+          );
+        })}
       </div>
     </>
   );
