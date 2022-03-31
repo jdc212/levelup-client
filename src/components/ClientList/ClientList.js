a25623abe713bcc2f6672066b552fbe3d8ab3bba
Larissa Ferreira <larissadantasaf@gmail.com>
2022-03-31 16:03:41 +0200

    Fix style

--- src/components/ClientList/ClientList.js
+++ src/components/ClientList/ClientList.js
@@ -1,7 +1,6 @@
 import api from "../../apis/api";
 import { useState } from "react";
 
-
 export function ClientList(props) {
   const [credit, setCredit] = useState({ points: 0 });
   const [compensate, setCompensate] = useState({ number: 0 });
@@ -45,80 +44,131 @@ export function ClientList(props) {
   }
 
   return (
-    <div className="max-w-full bg-white overflow-hidden shadow sm:rounded-md">
-      <div className="block text-sm font-medium text-gray-700">
-        <h4 className="">Customer Points</h4>
-        <div className="">
-        <div className="">
-          <div className="mt-1 w-130 flex rounded-md shadow-sm grid grid-cols-3 gap-6">
-            <section>
-              <p className="block text-indigo-600 xl">Customer Email: {props.customerEmail}</p>
-              <p className="block text-indigo-600">Points: {props.pointsAccumulated}</p>
-            </section>
+    <>
+      <div className="sm:block" aria-hidden="true">
+        <div className="py-5">
+          <div className="border-t border-gray-200" />
+        </div>
+      </div>
+      <div className="bg-gray-50">
+        <div className="mt-10 sm:mt-0">
+          <div className="md:grid md:grid-cols-2 md:gap-6">
+            <div className="md:col-span-1">
+              <div className="px-4 sm:px-0">
+                <h3 className="text-lg font-medium leading-6 text-gray-900">
+                  Customer Points
+                </h3>
+                <p className="mt-1 text-sm text-gray-600">
+                  Use a permanent address where you can receive mail.
+                </p>
+              </div>
 
-            <section >
-              <form onSubmit={handleSubmitCredit} className="block text-sm font-medium text-gray-700">
-                <h4>Credit Points</h4>
-                <label htmlFor="credit" >Choose Program</label>
-                <br></br>
-                <select
-                  id="credit"
-                  name="points"
-                  /*className="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 cursor-default placeholder-gray-500 text-gray-900 rounded-b-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm pr-2"*/
-                  className="relative w-full bg-white border border-gray-300 rounded-md pl-3 pr-10 py-2 text-left cursor-default focus:outline-none focus:ring-1 focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
-                  value={credit.points}
-                  
-                  onChange={handleChangeCredit}
-                >
-                  <option value=""></option>
-                  {props.services.map((service) => {
-                    return (
-                      <option
-                        key={service.service}
-                        value={Number(service.creditSystem)}
-                      >{`Promotion: ${service.service} 
-                   Points to Credit: ${service.creditSystem}`}</option>
-                    );
-                  })}
-                </select>
-                    <br></br> 
-                    <div className="mt-5"></div>
-                <button 
-                type="submit" className="inline-flex justify-center py-2 px-4 border border-transparent shadow-sm text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
-                >
-                  Credit
-                </button>
-              </form>
-
-            </section>
+              <div className="mt-5 md:mt-0 md:col-span-2">
+                <div className="shadow sm:rounded-md sm:overflow-hidden">
+                  <div className="px-4 py-4 bg-white sm:p-6">
+                    <div className="grid grid-cols-1 gap-6">
+                      <div className="col-span-1 sm:col-span-2">
+                        <div>
+                          <p className="block text-indigo-600">
+                            Customer Email: {props.customerEmail}
+                          </p>
+                        </div>
+                        <div>
+                          <p className="block  text-indigo-600">
+                            Points: {props.pointsAccumulated}
+                          </p>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
 
             <section>
-              <form
-                className="block text-sm font-medium text-gray-700"
-                onSubmit={handleSubmitCompensate}
-              >
-                <h4>Compensate Points</h4>
-                <label htmlFor="number" className="block text-sm font-medium text-gray-700">Points to Compensate</label>
-                <input
-                  id="number"
-                  name="number"
-                  className="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-b-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm"
-                  value={props.number}
-                  onChange={handleChangeCompensate}
-                />
-                <br></br>
-                <button 
-                type="submit" 
-                className="inline-flex justify-center py-2 px-4 border border-transparent shadow-sm text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
-                >
-                  Compensate
-                </button>
-              </form>
+              <div className="mt-5 md:mt-0 md:col-span-2">
+                <form onSubmit={handleSubmitCredit}>
+                  <div className="shadow sm:rounded-md sm:overflow-hidden">
+                    <div className="px-4 py-4 bg-white space-y-6 sm:p-6">
+                      <div className="grid grid-cols-1 gap-6">
+                        <div className="col-span-3 sm:col-span-2">
+                          <label
+                            htmlFor="credit"
+                            className="block text-sm font-medium text-gray-700"
+                          >
+                            Choose promotion and credit points
+                          </label>
+                          <select
+                            id="credit"
+                            name="points"
+                            className="relative w-full bg-white border border-gray-300 rounded-md pl-3 pr-10 py-2 text-left cursor-default focus:outline-none focus:ring-0 focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
+                            value={credit.points}
+                            onChange={handleChangeCredit}
+                          >
+                            <option value=""></option>
+                            {props.services.map((service) => {
+                              return (
+                                <option
+                                  key={service.service}
+                                  value={Number(service.creditSystem)}
+                                >{`Promotion: ${service.service} /
+                              Points to Credit: ${service.creditSystem}`}</option>
+                              );
+                            })}
+                          </select>
+                        </div>
+                      </div>
+                    </div>
+                    <div className="px-4 py-3 bg-gray-50 text-right sm:px-6">
+                      <button
+                        type="submit"
+                        className="inline-flex justify-center py-2 px-4 border border-transparent shadow-sm text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+                      >
+                        Credit
+                      </button>
+                    </div>
+                  </div>
+                </form>
+                <div className="mt-5 md:mt-0 md:col-span-2">
+                  <form onSubmit={handleSubmitCompensate}>
+                    <div className="shadow sm:rounded-md sm:overflow-hidden">
+                      <div className="px-4 py-4 bg-white space-y-6 sm:p-6">
+                        <div className="grid grid-cols-1 gap-6">
+                          <div className="col-span-3 sm:col-span-2">
+                            <label
+                              htmlFor="number"
+                              className="block text-sm font-medium text-gray-700"
+                            >
+                              Points to Compensate
+                            </label>
+                            <input
+                              id="number"
+                              name="number"
+                              type="number"
+                              className="relative w-full bg-white border border-gray-300 rounded-md px-3 py-2 text-left cursor-default focus:outline-none focus:ring-0 focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
+                              value={props.number}
+                              onChange={handleChangeCompensate}
+                            />
+                          </div>
+                        </div>
+                      </div>
+                      <div className="px-4 py-3 bg-gray-50 text-right sm:px-6">
+                        <button
+                          type="submit"
+                          className="inline-flex justify-center py-2 px-4 border border-transparent shadow-sm text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+                        >
+                          Compensate
+                        </button>
+                      </div>
+                    </div>
+                  </form>
+                </div>
+              </div>
             </section>
           </div>
         </div>
       </div>
-    </div>
-    </div>
+      <div className="sm:block py-5" aria-hidden="true" />
+    </>
   );
 }
